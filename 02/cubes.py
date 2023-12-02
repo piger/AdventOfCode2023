5e99699234fa49fdcb889b882cb8f82f8c185a1b
Daniel Kertesz <daniel@spatof.org>
2023-12-02 18:29:52 +0000

    solution 02 part 2

--- 02/cubes.py
+++ 02/cubes.py
@@ -43,6 +43,37 @@ def check_game(line):
             return 0
     return game_id
 
+def check_game_part2(line):
+    # Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    game_label, game_data = [x.strip() for x in line.split(":", 2)]
+    game_id = int(game_label.split(" ")[-1])
+    print(f"game_label = '{game_label}', game_data = '{game_data}', game_id: {game_id}")
+
+    # 3 blue, 4 red
+    games = [x.strip() for x in game_data.split(";")]
+
+    cubes = {}
+
+    for game in games:
+
+        # 3 blue
+        draws = [x.strip() for x in game.split(",")]
+
+        for draw in draws:
+            amount, color = draw.split(" ")
+            amount = int(amount)
+            if color not in cubes:
+                cubes[color] = amount
+            else:
+                if amount > cubes[color]:
+                    cubes[color] = amount
+
+    result = 1
+    for color, amount in cubes.items():
+        result = result * amount
+
+    print(result)
+    return result
 
 def main():
     if len(sys.argv) < 2:
@@ -55,7 +86,7 @@ def main():
     with open(filename) as fd:
         for line in fd:
             line = line.strip()
-            score = check_game(line)
+            score = check_game_part2(line)
             result += score
 
     print(f"result = {result}")
